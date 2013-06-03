0a3890ccd40d33a4e0679aaafce31fca0a089ede
stripes <stripes@minifig>
2013-06-02 22:30:23 -0700

    Fixed CIDR notation. Not sure why it didn't work, but it works now.
    Commented out TTL. Need to get working, and add OS dependency checks.

--- ping_sweep.py
+++ ping_sweep.py
@@ -2,11 +2,23 @@
 
 # ping_sweep.py - ping sweep a range of IPs or network block.
 #
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; Applies version 2 of the License.
+#
 # ping_sweep.py -r <first-ip>-<lastip> | -c <ip/mask> [-t timeout]
 # 
 # -r range of IPs
 # -c CIDR notation
 # -t default is 500 (milliseconds?)
+#
+# 6/3/2013 - Range and CIDR work now. 
+#
+# TODO:
+# 
+# Check per OS type (Mac/Linux/Windows)
+# Get TTL working (OS dependent, need defaults)
+#
 
 
 import os
@@ -14,14 +26,15 @@ import re
 import optparse
 import netaddr
 import subprocess
+import netaddr
 
 from netaddr import *
 from subprocess import Popen, PIPE, STDOUT
 
 parser = optparse.OptionParser()
 
-parser.add_option("--timeout","-t", help="define the timeout value",type=int)
 parser.add_option("--range","-r", help="define the IP range", nargs=1)
+#parser.add_option("--timeout","-t", help="define the timeout value",type=int)
 parser.add_option("--cidr","-c", help="define the network in CIDR notation",nargs=1)
 
 (opts, args) = parser.parse_args()
@@ -31,27 +44,24 @@ if not opts.range and not opts.cidr:
    exit(1)
 elif opts.cidr:
    iprange = IPNetwork(opts.cidr)
-   print "DEBUG: iprange = " + str(iprange)
+   #print "DEBUG: CIDR = " + str(iprange)
 elif opts.range:
    ips = opts.range.split("-")
    iprange = IPRange(ips[0], ips[1])
-   print "DEBUG: iprange = " + str(iprange)
 
-if opts.timeout is not None:
-   timeout = opts.timeout
-   print timeout
+## FIX
+#if opts.timeout is not None:
+#   timeout = opts.timeout
+#   print timeout
 
 for ip in iprange:
+   #print "DEBUG: IP = " + str(ip)
    netaddress = re.compile("^\d{1,3}.\d{1,3}.\d{1,3}.0")
    test = netaddress.match(str(ip))
    if not test:
      DEVNULL = open(os.devnull, 'wb')
-     response = subprocess.call(['/bin/ping', '-n', '-c', '1', str(ip)], \
+     response = subprocess.call(['/bin/ping', '-c', '1', str(ip)], \
          stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
      # If the IP Address answers, print it
      if response == 0:
-        print ip
-     #elif response == 2:
-     #   print "no response from", ip
-     #else:
-     #   print "ping to", ip, "failed!"
+         print ip
