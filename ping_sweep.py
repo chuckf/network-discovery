6c02084386c2cd9dbd4e3692656ba0efaebdc324
stripes <stripes@minifig>
2013-06-03 21:07:17 -0700

    Phase 1 complete :)

--- ping_sweep.py
+++ ping_sweep.py
@@ -22,6 +22,7 @@
 
 
 import os
+import platform
 import re
 import optparse
 import netaddr
@@ -31,37 +32,50 @@ import netaddr
 from netaddr import *
 from subprocess import Popen, PIPE, STDOUT
 
-parser = optparse.OptionParser()
+def main():
 
-parser.add_option("--range","-r", help="define the IP range", nargs=1)
-#parser.add_option("--timeout","-t", help="define the timeout value",type=int)
-parser.add_option("--cidr","-c", help="define the network in CIDR notation",nargs=1)
+  if "Win" in platform.system():
+     ostype = "windows"
+  elif "Darwin" or "FreeBSD" or "NetBSD" or "Linux" in platform.system():
+     ostype = "unix"
 
-(opts, args) = parser.parse_args()
+  parser = optparse.OptionParser()
 
-if not opts.range and not opts.cidr:
-   print "You need to provide either the range or CIDR notation."
-   exit(1)
-elif opts.cidr:
-   iprange = IPNetwork(opts.cidr)
-   #print "DEBUG: CIDR = " + str(iprange)
-elif opts.range:
-   ips = opts.range.split("-")
-   iprange = IPRange(ips[0], ips[1])
+  parser.add_option("--range","-r", help="define the IP range", nargs=1)
+  parser.add_option("--timeout","-t", help="define the timeout value in milliseconds",type=int)
+  parser.add_option("--cidr","-c", help="define the network in CIDR notation",nargs=1)
 
-## FIX
-#if opts.timeout is not None:
-#   timeout = opts.timeout
-#   print timeout
+  (opts, args) = parser.parse_args()
 
-for ip in iprange:
-   #print "DEBUG: IP = " + str(ip)
-   netaddress = re.compile("^\d{1,3}.\d{1,3}.\d{1,3}.0")
-   test = netaddress.match(str(ip))
-   if not test:
-     DEVNULL = open(os.devnull, 'wb')
-     response = subprocess.call(['/bin/ping', '-c', '1', str(ip)], \
-         stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
-     # If the IP Address answers, print it
-     if response == 0:
+  if not opts.range and not opts.cidr:
+     print "You need to provide either the range or CIDR notation."
+     exit(1)
+  elif opts.cidr:
+     iprange = IPNetwork(opts.cidr)
+  elif opts.range:
+     ips = opts.range.split("-")
+     iprange = IPRange(ips[0], ips[1])
+
+  if opts.timeout is not None:
+     timeout = opts.timeout
+  else:
+     timeout = None
+     
+  for ip in iprange:
+     netaddress = re.compile("^\d{1,3}.\d{1,3}.\d{1,3}.0")
+     test = netaddress.match(str(ip))
+     if not test:
+       DEVNULL = open(os.devnull, 'wb')
+       if timeout:  
+         response = subprocess.call(['/bin/ping', '-W', str(timeout), '-c', '1', str(ip)],\
+           stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+       else:
+         response = subprocess.call(['/bin/ping', '-c', '1', str(ip)], \
+           stdin=PIPE, stdout=DEVNULL, stderr=STDOUT)
+       # If the IP Address answers, print it
+       if response == 0:
          print ip
+
+
+if __name__ == "__main__":
+    main()
